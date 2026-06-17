b42705425cf2516d51bdda2d0e6c3bd803d5bfd7
Dan Devine <dandevine0@gmail.com>
2026-06-17 07:34:33 +0100

    fixes relative paths

--- components/x-head.view.php
+++ components/x-head.view.php
@@ -8,11 +8,11 @@
     <link href="https://fonts.googleapis.com/css2?family=Comme:wght@100..900&family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap"
           rel="stylesheet">
     <link rel="stylesheet" href="/assets/main.css">
-    <link rel="icon" type="image/png" href="assets/favicon/favicon-96x96.png" sizes="96x96"/>
-    <link rel="icon" type="image/svg+xml" href="assets/favicon/favicon.svg"/>
-    <link rel="shortcut icon" href="assets/favicon/favicon.ico"/>
-    <link rel="apple-touch-icon" sizes="180x180" href="assets/favicon/apple-touch-icon.png"/>
-    <link rel="manifest" href="assets/favicon/site.webmanifest"/>
+    <link rel="icon" type="image/png" href="/assets/favicon/favicon-96x96.png" sizes="96x96"/>
+    <link rel="icon" type="image/svg+xml" href="/assets/favicon/favicon.svg"/>
+    <link rel="shortcut icon" href="/assets/favicon/favicon.ico"/>
+    <link rel="apple-touch-icon" sizes="180x180" href="/assets/favicon/apple-touch-icon.png"/>
+    <link rel="manifest" href="/assets/favicon/site.webmanifest"/>
     <title>{{ $title ?? '' }}</title>
     <meta name="description"
           content="Unofficial League table of the Dillon Cinematic Universe family world cup sweepstake.">
